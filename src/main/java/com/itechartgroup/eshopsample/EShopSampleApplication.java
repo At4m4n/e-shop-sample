530814dcdacce5175385190322edb358dfd2f533
Uladzislau Leusha <Uladzislau.Leusha@itechart-group.com>
2020-12-01 11:57:10 +0300

    Initial commit

---
+++ src/main/java/com/itechartgroup/eshopsample/EShopSampleApplication.java
@@ -0,0 +1,13 @@
+package com.itechartgroup.eshopsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShopSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EShopSampleApplication.class, args);
+  }
+
+}
